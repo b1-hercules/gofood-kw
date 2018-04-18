329c0c70fdbbe9ca221f25e66e920b54b69d90d5
iqbal115705 <sugandhiiqbal@gmail.com>
2018-04-19 02:40:33 +0700

    routes

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,10 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get("/orders/", function (Request $request, Response $response){
+    $sql = "SELECT * FROM orders";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
